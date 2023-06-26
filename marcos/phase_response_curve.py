a2b4dd5cf1327e47e5903717ccb403a82e59e593
mwappner <marcoswappner@gmail.com>
2023-06-26 00:56:29 -0300

    PRC stuff done

--- marcos/phase_response_curve.py
+++ marcos/phase_response_curve.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import pandas as pd
 import numpy as np
-from scipy import interpolate
+from scipy import interpolate, signal
 
-from utils import contenidos, find_point_by_value, calc_mode, sort_by
+from utils import contenidos, find_point_by_value, calc_mode, sort_by, enzip
 import analysis_utils as au
 
 import pyabf
@@ -173,6 +173,8 @@ def my_find_peaks(times, data, period_percent=0.4, prominence=3):
 
 
 BASE_DIR = '/media/marcos/DATA/marcos/FloClock_data/phase_response_curve'
+BASE_DIR = '/home/user/Documents/Doctorado/Fly clock/FlyClock_data/phase_response_curve'
+
 files = contenidos(BASE_DIR, filter_ext='.abf')
 
 file = files[-2]
@@ -234,10 +236,11 @@ for sweep in abf.sweepList[:2]:
 
 
 BASE_DIR = '/media/marcos/DATA/marcos/FloClock_data/phase_response_curve'
+BASE_DIR = '/home/user/Documents/Doctorado/Fly clock/FlyClock_data/phase_response_curve'
 files = contenidos(BASE_DIR, filter_ext='.abf')
 
 file = files[19]
-threshold = 5
+threshold = 10
 
 # Load data
 abf = pyabf.ABF(file)
@@ -370,9 +373,204 @@ ax2.legend(loc='lower left')
 axp.legend(loc='upper left', ncol=4)
 axp.set_ylabel('period [sec]')
 
+
+#%% Try to get PRC
+
+def lowpass_filter(times, data, filter_order, frequency_cutoff):
+    
+    # some filter methods leave behind nans in the data, that raises a LinAlgError
+    nan_locs = np.isnan(data)
+            
+    sampling_rate = 1/(times[1]-times[0])
+    sos = signal.butter(filter_order, frequency_cutoff, btype='lowpass', output='sos', fs=sampling_rate)
+    filtered = signal.sosfiltfilt(sos, data[~nan_locs])
+    
+    return filtered
+    
+
+def highpass_filter(times, data, filter_order, frequency_cutoff):
+    
+    # some filter methods leave behind nans in the data, that raises a LinAlgError
+    nan_locs = np.isnan(data)
+            
+    sampling_rate = 1/(times[1]-times[0])
+    sos = signal.butter(filter_order, frequency_cutoff, btype='highpass', output='sos', fs=sampling_rate)
+    filtered = signal.sosfiltfilt(sos, data[~nan_locs])
+    
+    return filtered
+
+
+BASE_DIR = '/media/marcos/DATA/marcos/FloClock_data/phase_response_curve'
+BASE_DIR = '/home/user/Documents/Doctorado/Fly clock/FlyClock_data/phase_response_curve'
+files = contenidos(BASE_DIR, filter_ext='.abf')
+
+file = files[17]
+threshold = 8
+
+# Load data
+abf = pyabf.ABF(file)
+
+# to prime it for the first sweep
+times = [0]
+all_time = []
+all_data = []
+all_pert = []
+
+for sweep in abf.sweepList:
+    print('Processing sweep', sweep)
+
+    # first gaussian filter
+    abf.setSweep(sweep)
+    times = abf.sweepX + times[-1]
+    data = abf.sweepY
+    
+    all_time.append(times)
+    all_data.append(data)
+    all_pert.append(abf.sweepC)
+
+time = np.concatenate(all_time)
+data = np.concatenate(all_data)
+perturbation = np.concatenate(all_pert)
+
+# filter data a bit
+data_lp = lowpass_filter(time, data, filter_order=2, frequency_cutoff=10)
+data_lp = data_lp[::10]
+time = time[::10]
+data = data[::10]
+perturbation = perturbation[::10]
+data_hp = highpass_filter(time, data_lp, filter_order=2, frequency_cutoff=0.1)
+data_hplp = lowpass_filter(time, data_lp, filter_order=2, frequency_cutoff=2)
+
+
+# plot
+fig, (ax, ax2, axp) = plt.subplots(3, sharex=True, figsize=[17.69,  7.29], constrained_layout=True)
+
+ax.plot(time, data)
+ax.plot(time, perturbation + data.mean())
+ax.plot(time, data_lp)
+
+ax2.plot(time, data_hp)
+ax2.plot(time, perturbation + data_hp.mean())
+
+data = data_hp
+# find peaks and edge crossings
+
+peaks = my_find_peaks(time, data)
+ax2.plot(time[peaks], data[peaks], 'o')
+
+mean_period_in_points = round(np.diff(peaks).mean())
+
+# find crossover points
+rising_edge = []
+falling_edge = []
+filtered_peaks = []
+prev_peak = -np.inf
+for peak in peaks:
+    
+    # skip maxima that are too low
+    if data[peak] < threshold:
+        continue
+    
+    # skip maxima that are too close together
+    if peak - prev_peak < mean_period_in_points / 2:
+        continue
+    
+    # find rising edge point (before peak)
+    interval = data[:peak]
+    raising = np.nonzero(interval < threshold)[0][-1]
+    
+    # find falling edge point (after peak)    
+    starting_point = min(peak + int(mean_period_in_points / 2), len(data))
+    interval = data[:starting_point]
+    falling = np.nonzero(interval > threshold)[0][-1]
+    
+    rising_edge.append(raising)
+    falling_edge.append(falling)
+    filtered_peaks.append(peak)
+    prev_peak = peak
+
+rising_edge = np.asarray(rising_edge)
+falling_edge = np.asarray(falling_edge)
+peaks = np.asarray(filtered_peaks)
+
+# plot peaks and threshold crossings
+ax2.plot(time[peaks], data[peaks], 'ko')
+ax2.plot(time[rising_edge], data[rising_edge], 'o')
+
+# find cycles with a perturbation
+pert_inxs,  = np.where(np.diff(perturbation) < -1)
+pert_moments = time[pert_inxs]
+ax2.plot(pert_moments, perturbation[pert_inxs], 'kx')
+
+cross_moments = time[rising_edge]
+
+perturbed_cylces = []
+perturb_phase = []
+try:
+    for i, (re1, re2) in enzip(rising_edge, rising_edge[1:]):
+        # find the first perturbation after the current crossing
+        for pert in pert_inxs:
+            if pert > re1:
+                break
+        else:
+            raise StopIteration()
+        
+        # if te perturbation happened before the next peak, store it
+        if pert <= re2:
+            perturbed_cylces.append(i)
+            
+            # get phase of perturbation
+            phase = (pert-re1) / (re2-re1)
+            perturb_phase.append(phase)
+            
+except StopIteration:
+    # this happens if there are no more perturbations to find
+    pass
+
+# mark the cycles that have a perturbation
+for inx in perturbed_cylces:
+    start = rising_edge[inx]
+    end = rising_edge[inx+1]
+    ax2.plot(time[start:end], data_hp[start:end], 'r')
+    
+for pi, ph in zip(pert_inxs, perturb_phase):
+    ax2.text(time[pi], perturbation[pi]+0.5, f'{ph:.2f}')
+
+# calculate and store the (normalzied) Δipi
+pert_dipi = []
+prev_dipi = []
+for inx in perturbed_cylces:
+
+    re_2 = rising_edge[inx-2]
+    re_1 = rising_edge[inx-1]
+    re0 = rising_edge[inx]
+    re1 = rising_edge[inx+1]
+    
+    pert_ipi = time[re1] - time[re0]
+    base_ipi = time[re0] - time[re_1]
+    prev_ipi = time[re_1] - time[re_2]
+
+    pert_dipi.append( (pert_ipi - base_ipi) / base_ipi )
+    prev_dipi.append( (prev_ipi - base_ipi) / base_ipi )
+    
+pert_dipi = np.asarray(pert_dipi)
+prev_dipi = np.asarray(prev_dipi)
+dipi_times = time[rising_edge[perturbed_cylces]]
+
+# plot them
+axp.plot(dipi_times, pert_dipi, 'x')
+axp.plot(dipi_times, prev_dipi, '+')
+
+# plot PRC
+fig2, ax_prc = plt.subplots()
+
+ax_prc.plot(perturb_phase, pert_dipi, 'x')
+ax_prc.plot(perturb_phase, prev_dipi, '+')
+
 #%%
 
 file = '/media/marcos/DATA/marcos/FloClock_data/Raw data preps con ojos/14320023.abf'
+# file = '/home/user/Documents/Doctorado/Fly clock/FlyClock_data/phase_response_curve'
 
 abf = pyabf.ABF(file)
 abf.setSweep(0)
